0798482e3a42a3a352573ef649fd44c6b07e8cfb
Max Caplow <achipmunkdev@gmail.com>
2015-07-23 10:49:12 -0700

    Added comments

--- image.py
+++ image.py
@@ -4,15 +4,15 @@ import csv
 
 r = csv.reader(open("ScumScan.csv"))
 ScumWorkingData = [l for l in r]
+#ScumWorkingData is a list of the data loaded from the CSV that can manipulated, then later saved in full
 
-#wr = csv.writer(open('ScumScanOutput.csv','wb'))
-
+#Displays the croped version of the image. May require XV on non-mac opperating systems. Takes the full path and pixel extermities as input.
 def display(imgPath, x_min, y_min, x_max, y_max):
 
 	im = Image.open(imgPath)
-	#print im.format, im.size, im.mode
 	im.crop((x_min, y_min, x_max, y_max)).show()
 
+#Definitions for taxonomy. Taken from http://www.msnucleus.org/watersheds/mission/plankton.pdf and number in order.
 def identify(idNum):
 	if idNum == 1:
 		outp = ("null","Chlorophyceae","null","Green algae (with flagella, small)")
@@ -58,30 +58,40 @@ def identify(idNum):
 		outp = ("null","Platyhelminthes","null","Flatworms")
 	return outp
 
-
+#ask for a starting row so that you don't need to start from the top each time.
 startingRow = input("what row would you like to start with?  ")
+
+#When doContinue is set to false, the program will terminate.
 doContinue = True 
+
+#counter + startingRow will always be the current row.
 counter = 0
+
 while doContinue:
 	curRow = startingRow + counter
+
+	#Checking to make sure at least 1 field in the taxonomy section is blank, otherwise the row is skipped.
 	if str(ScumWorkingData[curRow][13]) == "" or str(ScumWorkingData[curRow][14]) == "" or str(ScumWorkingData[curRow][15]) == "" or str(ScumWorkingData[curRow][16]) == "":
 
+		#shows inmage to user.
 		display(ScumWorkingData[curRow][17],int(ScumWorkingData[curRow][8]),int(ScumWorkingData[curRow][10]),int(ScumWorkingData[curRow][9]),int(ScumWorkingData[curRow][11]))
 
+		#askes if user recognises image. if not, the next image is shown
 		doesRecognize = raw_input("do you recognize this object? (y/n) ")
 
 
 		if doesRecognize != "n":
+
+			#asks for an idenification from the PDF file, otherwise goes to manual input.
 			databaseNumber = input("Can you identify the entry on the key? (1-?, or n)")
 			if databaseNumber != "n":
 				identity = identify(databaseNumber)
-				print identity[0]
+				#update ScumWorkingData with the retunred values from hte identity function.
 				ScumWorkingData[curRow][13] = identity[0]
 				ScumWorkingData[curRow][14] = identity[1]
 				ScumWorkingData[curRow][15] = identity[2]
 				ScumWorkingData[curRow][16] = identity[3]
 				
-				#(ScumWorkingData[curRow][13], ScumWorkingData[curRow][14], ScumWorkingData[curRow][15], ScumWorkingData[curRow][16]) = identify(databaseNumber)
 
 			else:	
 				order = raw_input("order? ")
@@ -97,11 +107,13 @@ while doContinue:
 				if spp != "":
 					ScumWorkingData[curRow][16] = spp
 
+		#makes sure user wants to continue.			
 		if str(raw_input('continue? (y/n)  ')) == 'n':
 			doContinue = False
 
 	counter = counter + 1
 
+	#Writes updated ScumWorkingData to new CSV.
 	wr = csv.writer(open('ScumScanOutput.csv','wb'))
 	wr.writerows(ScumWorkingData)
 print "Thank you! You saw ", counter, " rows!"
